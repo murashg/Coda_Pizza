d0763e354dcc00a85b6e2dd0bb0e003fa9df53d8
murashg <gregmurashige@gmail.com>
2024-11-03 22:11:26 -0800

    Enum classes

---
+++ app/src/main/java/android/bignerdranch/codapizza/model/ToppingPlacement.kt
@@ -0,0 +1,11 @@
+package android.bignerdranch.codapizza.model
+
+import androidx.annotation.StringRes
+
+enum class ToppingPlacement(
+    @StringRes val label: Int
+) {
+    Left(R.string.placement_left),
+    Right(R.string.placement_right),
+    All(R.string.placement_all)
+}
