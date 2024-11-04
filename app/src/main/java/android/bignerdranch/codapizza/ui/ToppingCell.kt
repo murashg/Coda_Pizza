6a5d3cdb73d2113e7ec737548396f0aeac091f23
murashg <gregmurashige@gmail.com>
2024-11-04 08:17:30 -0800

    Optimize imports

--- app/src/main/java/android/bignerdranch/codapizza/ui/ToppingCell.kt
+++ app/src/main/java/android/bignerdranch/codapizza/ui/ToppingCell.kt
@@ -2,7 +2,6 @@ package android.bignerdranch.codapizza.ui
 
 import android.bignerdranch.codapizza.model.Topping
 import android.bignerdranch.codapizza.model.ToppingPlacement
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
