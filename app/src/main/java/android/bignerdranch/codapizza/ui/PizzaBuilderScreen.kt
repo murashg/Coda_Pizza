dfed33aad436ce216460f8b4ff1cfcd1343baf36
murashg <gregmurashige@gmail.com>
2024-11-04 21:18:11 -0800

    Remove global var pizza to remembered mutable state so encapsulate state inside of composable functions.

--- app/src/main/java/android/bignerdranch/codapizza/ui/PizzaBuilderScreen.kt
+++ app/src/main/java/android/bignerdranch/codapizza/ui/PizzaBuilderScreen.kt
@@ -4,7 +4,6 @@ import android.bignerdranch.codapizza.R
 import android.bignerdranch.codapizza.model.Pizza
 import android.bignerdranch.codapizza.model.Topping
 import android.bignerdranch.codapizza.model.ToppingPlacement
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,6 +16,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Preview
 @Composable
@@ -39,22 +42,11 @@ fun PizzaBuilderScreen(
     }
 }
 
-private var pizza =
-    Pizza(
-        toppings = mapOf(
-            Topping.Pepperoni to ToppingPlacement.All,
-            Topping.Pineapple to ToppingPlacement.All
-        )
-    )
-    set(value) {
-        Log.d("PizzaBuilderScreen", "Reassigned pizza to $value")
-        field = value
-    }
-
 @Composable
 private fun ToppingsList(
     modifier: Modifier = Modifier
 ){
+    var pizza by remember { mutableStateOf(Pizza()) }
     LazyColumn(
         modifier = modifier
     ) {
