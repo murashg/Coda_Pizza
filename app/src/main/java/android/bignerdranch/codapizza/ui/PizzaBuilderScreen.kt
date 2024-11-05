8f0fc9b1d2739f86d2e1893124034920fc6e244c
murashg <gregmurashige@gmail.com>
2024-11-04 20:42:34 -0800

    LazyColumn for scrollable list of toppings

--- app/src/main/java/android/bignerdranch/codapizza/ui/PizzaBuilderScreen.kt
+++ app/src/main/java/android/bignerdranch/codapizza/ui/PizzaBuilderScreen.kt
@@ -13,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 
 @Preview
 @Composable
@@ -39,12 +41,20 @@ fun PizzaBuilderScreen(
 private fun ToppingsList(
     modifier: Modifier = Modifier
 ){
-    ToppingCell(
-        topping = Topping.Pepperoni,
-        placement = ToppingPlacement.Left,
-        onClickTopping = {},
+    LazyColumn(
         modifier = modifier
-    )
+    ) {
+        items(Topping.values()) { topping ->
+            ToppingCell(
+                topping = topping,
+                placement = ToppingPlacement.Left,
+                onClickTopping = {
+                    //TODO
+                }
+            )
+
+        }
+    }
 }
 
 @Composable
