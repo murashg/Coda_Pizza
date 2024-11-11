c4ceb4420b11d9bd0a5f328b5d8da148dfdc7836
murashg <gregmurashige@gmail.com>
2024-11-10 21:05:39 -0800

    Add toppingdialog useage to pizzabuilderscreen

--- app/src/main/java/android/bignerdranch/codapizza/ui/PizzaBuilderScreen.kt
+++ app/src/main/java/android/bignerdranch/codapizza/ui/PizzaBuilderScreen.kt
@@ -3,24 +3,23 @@ package android.bignerdranch.codapizza.ui
 import android.bignerdranch.codapizza.R
 import android.bignerdranch.codapizza.model.Pizza
 import android.bignerdranch.codapizza.model.Topping
-import android.bignerdranch.codapizza.model.ToppingPlacement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import java.text.NumberFormat
 
 @Preview
@@ -54,7 +53,15 @@ private fun ToppingsList(
     onEditPizza: (Pizza) -> Unit,
     modifier: Modifier = Modifier
 ){
-    ToppingPlacementDialog()
+    var toppingBeingAdded by rememberSaveable { mutableStateOf<Topping?>(null) }
+    toppingBeingAdded?.let { topping ->
+        ToppingPlacementDialog(
+            topping = topping,
+            onDismissRequest = {
+                toppingBeingAdded = null
+            }
+        )
+    }
     LazyColumn(
         modifier = modifier
     ) {
@@ -63,15 +70,7 @@ private fun ToppingsList(
                 topping = topping,
                 placement = pizza.toppings[topping],
                 onClickTopping = {
-                    val isOnPizza = pizza.toppings[topping] != null
-                    onEditPizza(pizza.withTopping(
-                        topping = topping,
-                        placement = if (isOnPizza) {
-                            null
-                        } else {
-                            ToppingPlacement.All
-                        }
-                    ))
+                    toppingBeingAdded = topping
                 }
             )
         }
--- app/src/main/java/android/bignerdranch/codapizza/ui/ToppingPlacementDialog.kt
+++ app/src/main/java/android/bignerdranch/codapizza/ui/ToppingPlacementDialog.kt
@@ -1,21 +1,32 @@
 package android.bignerdranch.codapizza.ui
 
-import androidx.compose.ui.window.Dialog
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
+import android.bignerdranch.codapizza.R
+import android.bignerdranch.codapizza.model.Topping
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 
 @Composable
-fun ToppingPlacementDialog() {
-    Dialog(onDismissRequest = { /* TODO */}) {
-        Box(
-            modifier = Modifier
-                .background(Color.Red)
-                .size(64.dp)
-        )
+fun ToppingPlacementDialog(
+    topping: Topping,
+    onDismissRequest: () -> Unit
+) {
+    Dialog(onDismissRequest = { onDismissRequest }) {
+        Card {
+            val toppingName = stringResource(topping.toppingName)
+            Text(
+                text = stringResource(R.string.placement_prompt, toppingName),
+                style = MaterialTheme.typography.titleMedium,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(24.dp)
+            )
+        }
     }
 }
