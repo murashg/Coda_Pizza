b8cbb245457c5870527324112c11df259da992ba
murashg <gregmurashige@gmail.com>
2024-11-10 21:29:25 -0800

    ToppingPlacementOptions

--- app/src/main/java/android/bignerdranch/codapizza/ui/ToppingPlacementDialog.kt
+++ app/src/main/java/android/bignerdranch/codapizza/ui/ToppingPlacementDialog.kt
@@ -2,10 +2,14 @@ package android.bignerdranch.codapizza.ui
 
 import android.bignerdranch.codapizza.R
 import android.bignerdranch.codapizza.model.Topping
+import android.bignerdranch.codapizza.model.ToppingPlacement
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -18,15 +22,49 @@ fun ToppingPlacementDialog(
     topping: Topping,
     onDismissRequest: () -> Unit
 ) {
-    Dialog(onDismissRequest = { onDismissRequest }) {
+    Dialog(onDismissRequest = onDismissRequest) {
         Card {
             val toppingName = stringResource(topping.toppingName)
             Text(
                 text = stringResource(R.string.placement_prompt, toppingName),
                 style = MaterialTheme.typography.titleMedium,
                 textAlign = TextAlign.Center,
-                modifier = Modifier.padding(24.dp)
+                modifier = Modifier
+                    .padding(24.dp)
+            )
+            ToppingPlacement.entries.forEach { placement ->
+                ToppingPlacementOption(
+                    placementName = placement.label,
+                    onClick = {
+                        onDismissRequest()
+                    }
+                )
+            }
+            ToppingPlacementOption(
+                placementName = R.string.placement_none,
+                onClick = {
+                    onDismissRequest()
+                }
             )
         }
+    }
+}
+
+@Composable
+private fun ToppingPlacementOption(
+    @StringRes placementName: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextButton(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = stringResource(placementName),
+            modifier = Modifier
+                .padding(8.dp)
+        )
     }
 }
