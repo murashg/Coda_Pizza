54d9774f29f4c259c1746efcca9de951032947cd
murashg <gregmurashige@gmail.com>
2024-11-13 14:29:19 -0800

    Optimize imports

--- app/src/main/java/android/bignerdranch/codapizza/ui/OrderButton.kt
+++ app/src/main/java/android/bignerdranch/codapizza/ui/OrderButton.kt
@@ -5,7 +5,6 @@ import android.bignerdranch.codapizza.model.Pizza
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import java.text.NumberFormat
