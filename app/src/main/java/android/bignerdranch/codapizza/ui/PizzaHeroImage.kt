6eb56ba4d52ac03bc070db367269b0c8e69ef843
murashg <gregmurashige@gmail.com>
2024-11-13 12:35:13 -0800

    Content Scale Crop

--- app/src/main/java/android/bignerdranch/codapizza/ui/PizzaHeroImage.kt
+++ app/src/main/java/android/bignerdranch/codapizza/ui/PizzaHeroImage.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,6 +38,7 @@ fun PizzaHeroImage(
             Image(
                 painter = painterResource(topping.pizzaOverlayImage),
                 contentDescription = null,
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .focusable(false)
                     .aspectRatio(when (placement) {
