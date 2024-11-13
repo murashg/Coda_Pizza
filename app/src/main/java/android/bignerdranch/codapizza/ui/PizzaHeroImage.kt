08dd8ba3a8278e12fdc5a4ea1ba6582ed1cc3c12
murashg <gregmurashige@gmail.com>
2024-11-13 12:38:53 -0800

    Toppings preview correctly, now to put into app

--- app/src/main/java/android/bignerdranch/codapizza/ui/PizzaHeroImage.kt
+++ app/src/main/java/android/bignerdranch/codapizza/ui/PizzaHeroImage.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -39,11 +40,21 @@ fun PizzaHeroImage(
                 painter = painterResource(topping.pizzaOverlayImage),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
+                alignment = when (placement) {
+                    Left -> Alignment.TopStart
+                    Right -> Alignment.TopEnd
+                    All -> Alignment.Center
+                },
                 modifier = Modifier
                     .focusable(false)
                     .aspectRatio(when (placement) {
                         Left, Right -> 0.5f
                         All -> 1.0f
+                    })
+                    .align(when (placement) {
+                        Left -> Alignment.CenterStart
+                        Right -> Alignment.CenterEnd
+                        All -> Alignment.Center
                     })
             )
         }
