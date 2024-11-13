3f91ff676b675db533a7878e765082f01348e57e
murashg <gregmurashige@gmail.com>
2024-11-13 12:18:44 -0800

    Init for hero image

---
+++ app/src/main/java/android/bignerdranch/codapizza/ui/PizzaHeroImage.kt
@@ -0,0 +1,37 @@
+package android.bignerdranch.codapizza.ui
+
+import android.bignerdranch.codapizza.R
+import android.bignerdranch.codapizza.model.Pizza
+import android.bignerdranch.codapizza.model.Topping
+import android.bignerdranch.codapizza.model.ToppingPlacement
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PizzaHeroImage(
+    pizza: Pizza,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = painterResource(R.drawable.pizza_crust),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+private fun PizzaHeroImagePreview() {
+    PizzaHeroImage(
+        pizza = Pizza(
+            toppings = mapOf(
+                Topping.Pineapple to ToppingPlacement.All,
+                Topping.Pepperoni to ToppingPlacement.Left,
+                Topping.Basil to ToppingPlacement.Right
+            )
+        )
+    )
+}
